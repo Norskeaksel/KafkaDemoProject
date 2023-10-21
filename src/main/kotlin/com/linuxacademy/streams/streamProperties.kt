15bd0e4c1e13e18d1c3f8ad1db156c7eee69a49d
Aksel <ahsorbye@gmail.com>
2023-10-22 00:55:32 +0200

    Refactor stream Properties

--- src/main/kotlin/com/linuxacademy/streams/streamProperties.kt
+++ src/main/kotlin/com/linuxacademy/streams/streamProperties.kt
@@ -1,5 +1,6 @@
 package com.linuxacademy.streams
 
+import ConfigVariables.BOOTSTRAP_SERVERS
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsConfig
 import java.util.*
@@ -7,9 +8,8 @@ import java.util.*
 fun setUpStream(): Properties {
     val props = Properties()
     props[StreamsConfig.APPLICATION_ID_CONFIG] = "MyStreamsGroup"
-    props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:29092"
+    props[StreamsConfig.BOOTSTRAP_SERVERS_CONFIG] = BOOTSTRAP_SERVERS
     props[StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
     props[StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG] = Serdes.String().javaClass.name
-    props.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0) // Disable caching. Good for testing, bad for production.
     return props
 }
