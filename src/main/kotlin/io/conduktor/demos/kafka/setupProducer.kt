e45ffaaa7bd22b8edae319113bf2c27a1c360dec
Aksel <ahsorbye@gmail.com>
2023-11-16 22:20:42 +0100

    Add setup comments

--- src/main/kotlin/io/conduktor/demos/kafka/setupProducer.kt
+++ src/main/kotlin/io/conduktor/demos/kafka/setupProducer.kt
@@ -2,5 +2,5 @@ package io.conduktor.demos.kafka
 import org.apache.kafka.clients.producer.KafkaProducer
 
 fun setupProducer(): KafkaProducer<String, String> {
-    TODO()
+    TODO() // Make, set and return properties
 }
