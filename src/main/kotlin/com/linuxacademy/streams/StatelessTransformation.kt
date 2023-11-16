f56bb8e131b29977b0f90e24ab59bcd1a9b70d3a
Aksel <ahsorbye@gmail.com>
2023-11-16 22:59:42 +0100

    Remove StatelessStream

--- src/main/kotlin/com/linuxacademy/streams/StatelessTransformation.kt
+++ src/main/kotlin/com/linuxacademy/streams/StatelessTransformation.kt
@@ -1,10 +1,7 @@
 package com.linuxacademy.streams
 
 import org.apache.kafka.streams.KafkaStreams
-import org.apache.kafka.streams.KeyValue
-import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.Topology
-import org.apache.kafka.streams.kstream.KStream
 
 object StatelessTransformation {
     @JvmStatic
@@ -16,21 +13,6 @@ object StatelessTransformation {
     }
 
     private fun buildTopology(): Topology {
-        val builder = StreamsBuilder()
-        val source: KStream<String?, String> = builder.stream("demo_topic")
-        val evenPredicate: (String?, String?) -> Boolean =
-            { _, value -> (value?.last()?.code ?: 1) % 2 == 0 && value?.last()?.code != 4 }
-
-        val evenStream = source.filter(evenPredicate)
-        val oddStream = source.filterNot(evenPredicate)
-        val foursStream = source.filter { _, value -> value.contains('4') }
-            .map { key, value -> KeyValue.pair(key, value.uppercase()) }
-
-        oddStream.to("odd-output-topic")
-        val refinedEvenStream = evenStream.merge(foursStream)
-        refinedEvenStream.to("even-output-topic")
-        val topology = builder.build()
-        println(topology.describe())
-        return topology
+        TODO()
     }
 }
